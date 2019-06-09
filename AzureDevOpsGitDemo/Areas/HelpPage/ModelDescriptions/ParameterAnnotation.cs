5dad3d4610b4088464de57cfc2197c2929d9cf0e
gauravpatni123 <gauravpaatni@gmail.com>
2019-06-09 14:54:05 +0530

    Add project files.

---
+++ AzureDevOpsGitDemo/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureDevOpsGitDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ AzureDevOpsGitDemo/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AzureDevOpsGitDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
