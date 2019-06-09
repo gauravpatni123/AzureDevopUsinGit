b5aafa00cfa73411e3fc6d2b6226a0b863bcb69b
gauravpatni123 <gauravpaatni@gmail.com>
2019-06-09 18:01:59 +0530

    Push Changes

--- AzureDevOpsGitDemo/Controllers/ValuesController.cs
+++ AzureDevOpsGitDemo/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace AzureDevOpsGitDemo.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            /*Test 1 2 3 4 5 6*/
+            /*Test 1 2 3 4 5 6 7*/
             return new string[] { "value1", "value2" };
         }
 
